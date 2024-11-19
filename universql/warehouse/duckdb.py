e810a5df0fac4e6535d1e265ef91b05bd75ac6cc
Dylan Ray <72663673+dylanray93@users.noreply.github.com>
2024-11-18 18:24:35 -0700

    Update duckdb.py
    update table creation logic

--- universql/warehouse/duckdb.py
+++ universql/warehouse/duckdb.py
@@ -387,10 +387,13 @@ class DuckDBExecutor(Executor):
             f"CREATE SCHEMA IF NOT EXISTS {sqlglot.exp.parse_identifier(db).sql()}.{sqlglot.exp.parse_identifier(schema).sql()}"
             for
             (db, schema) in schemas]
-        views_sql = [f"CREATE OR REPLACE TABLE {table.sql()} AS SELECT * FROM {self.get_iceberg_read(location)}" for
+        views_sql = [f"CREATE OR REPLACE VIEW {table.sql()}_V AS SELECT * FROM {self.get_iceberg_read(location)}" for
                      table, location
                      in locations.items()]
-        views_sql = ";\n".join(databases_sql + schemas_sql + views_sql)
+        tables_sql = [F"CREATE OR REPLACE TABLE {table.sql()} AS SELECT * FROM {table.sql()}_V" for
+                      table
+                      in locations.items()]
+        views_sql = ";\n".join(databases_sql + schemas_sql + views_sql + tables_sql)
 
         if views_sql != "":
             logger.info(
@@ -497,7 +500,7 @@ class DuckDBExecutor(Executor):
                         properties.expressions.append(
                             Property(this=Var(this='METADATA_FILE_PATH'), value=Literal.string(metadata_file_path)))
                         return {destination_table: ast}
-                elif ast.kind == 'TABLE':
+                elif ast.kind == 'VIEW':
                     properties = destination_table.args.get('properties') or Properties()
                     is_temp = TemporaryProperty() in properties.expressions
                     duckdb_query = self._sync_catalog(ast, tables).sql(dialect="duckdb")
